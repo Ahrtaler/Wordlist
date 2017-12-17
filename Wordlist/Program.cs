ff35fd2323a8f6f2d580f043659622f2924770f9
David Nelles <david.nelles@dlr.de>
2017-12-17 21:32:25 +0100

    Projektdateien hinzufügen.

---
+++ Wordlist/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Wordlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        string Website_durchsuchen()
+        {
+            WebClient client = new WebClient();
+
+            string st = client.DownloadString(https://de.wikipedia.org/wiki/Eichen);
+        }
+
+
+    }
+}
