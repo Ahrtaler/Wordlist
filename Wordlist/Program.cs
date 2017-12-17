393ce4723a3682c0d7a237c14a249536b7f743b0
David Nelles <david.nelles@dlr.de>
2017-12-18 00:09:41 +0100

    Herunterladen des Strings & Text zwischen <> herausfiltern

--- Wordlist/Program.cs
+++ Wordlist/Program.cs
@@ -11,16 +11,36 @@ namespace Wordlist
     {
         static void Main(string[] args)
         {
-
+            Website_durchsuchen();
         }
 
-        string Website_durchsuchen()
+        static void Website_durchsuchen()
         {
             WebClient client = new WebClient();
 
-            string st = client.DownloadString(https://de.wikipedia.org/wiki/Eichen);
+            string site = Text_filtern(client.DownloadString("https://de.wikipedia.org/wiki/Eichen"));
         }
 
+        static string Text_filtern(string site)
+        {
+            int start = 0;
 
+            for (int i = 0; i < site.Length; i++)
+            {
+                char c = site[i];
+                switch(c)
+                {
+                    case '<':
+                        start = i;
+                        break;
+                    case '>':
+                        site = site.Substring(0, start) + site.Substring(i + 1);
+                        i = i - (i - start) - 1;
+                        start = 0;
+                        break;
+                }
+            }
+            return site;
+        }
     }
 }
